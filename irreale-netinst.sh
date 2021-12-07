9bf885b40b6823b9c02b285fa1e49bd01d0d7e2f
zervbot <asanomo@outlook.jp>
2021-12-07 10:36:52 +0800

    Update irreale-netinst.sh

--- irreale-netinst.sh
+++ irreale-netinst.sh
@@ -2,11 +2,26 @@
 # Name : irreale network installer shell-script
 # Author : Harvey Wu
 
-# This is a script that can dowload irreale and modules from internet.
-# It runs on Unix-like operating systems (Linux, Macos, etc.), if you 
-# are currently using Windows please download the irreale installer.
+# This is a script that can dowload 'irreale' from internet. It runs on 
+# Unix-like operating systems (Linux, Macos, etc.), if you are currently
+# using Windows please download the irreale installation package.
 
 VERSION = "0.1.0"
+BASE_URL = ""
+INSTALL_DIR = "~/.irreale"
+CONFIG_DIR = "~/.config/irreale/"
 
 echo "irreale network installer v ${VERSION}"
-echo "Welcome!"
+
+main(){
+  env_check
+}
+
+# Check the environment.
+env_check(){
+  env = (git mkdir rmdir rm chmod python clang rustc cargo make)
+  for i in ${#env[*]}
+  do
+    command -v "${env[i]}" > /dev/null 2>&1
+  done
+}
